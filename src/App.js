2e6dccb94239ae9d9d2642d21e33118efdf28def
Peter Conley <peterdavidconley@gmail.com>
2021-12-01 16:11:36 -0500

    moved all slices of state from App.js to useDarkMode then imported that hook in with the proper variables to pass down into components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import useDarkMode from './Hooks/useDarkMode'
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
@@ -7,17 +8,9 @@ import Navbar from "./components/Navbar";
 import "./style.css";
 
 const App = () => {
-  const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
 
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-      )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
-  }, []);
+  const [ coinData, darkMode, setDarkMode ] = useDarkMode()
+  
   return (
     <div className={darkMode ? "dark-mode App" : "App"}>
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
